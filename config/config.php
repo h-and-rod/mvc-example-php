0ca93c8a8ef99c2c27735137bf1835a25d4b2f75
Henrique A. R. <henrique4r.it@gmail.com>
2025-09-08 09:34:11 -0300

    feat: implement configuration management
    Add config.php containing:
    - Database credentials (host, name, user, password)
    - Application base URL
    - Ready for dependency injection throughout MVC structure

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    "base_url"=> "http://localhost/mvc_portfolio/public/",
+    "db_host"=> "localhost",
+    "db_name"=> "portfolio",
+    "db_user"=> "root",
+    "db_pass"=> ""
+];
+?>
